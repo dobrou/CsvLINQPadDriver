2c562f2fa4b1458995111dd2fa6edd66206f2362
dobrou <dobrou@gmail.com>
2014-01-25 00:23:24 +0100

    Table cache improvements

--- Src/Src/CodeGen/CsvTableBase.cs
+++ Src/Src/CodeGen/CsvTableBase.cs
@@ -9,10 +9,16 @@ using System.Linq;
 namespace CsvLINQPadDriver.CodeGen
 {
 
-    public class CsvTableBase<TRow> : IEnumerable<TRow> where TRow : CsvRowBase, new()
+    public class CsvTableBase
     {
-        private readonly bool isDataCached;
-        private readonly Lazy<IEnumerable<TRow>> dataCache; //TODO consider using WeakReference
+        public static bool IsDataCached = true;
+        public static bool IsDataCachedStatic = false;
+    }
+
+    public class CsvTableBase<TRow> : CsvTableBase, IEnumerable<TRow> where TRow : CsvRowBase, new()
+    {
+        private readonly Lazy<IEnumerable<TRow>> dataCache;
+        private static IEnumerable<TRow> dataCacheStatic = null;
 
         internal readonly ICollection<CsvColumnInfo> PropertiesInfo;
         internal readonly Action<TRow> RelationsInit;
@@ -20,13 +26,12 @@ namespace CsvLINQPadDriver.CodeGen
         public char CsvSeparator { get; private set; }
         public string FilePath { get; private set; }
 
-        public CsvTableBase(char csvSeparator, string filePath, ICollection<CsvColumnInfo> propertiesInfo, Action<TRow> relationsInit, bool isDataCached = true)
+        public CsvTableBase(char csvSeparator, string filePath, ICollection<CsvColumnInfo> propertiesInfo, Action<TRow> relationsInit)
         {
             this.CsvSeparator = csvSeparator;
             this.FilePath = filePath;
             this.PropertiesInfo = propertiesInfo;
             this.RelationsInit = relationsInit;
-            this.isDataCached = isDataCached;
             this.dataCache = new Lazy<IEnumerable<TRow>>(() => GetDataDirect().ToList(), LazyThreadSafetyMode.ExecutionAndPublication);
         }
 
@@ -37,23 +42,25 @@ namespace CsvLINQPadDriver.CodeGen
 
         private IEnumerable<TRow> GetDataCached()
         {
-            return dataCache.Value;
+            if (IsDataCachedStatic)
+            {
+                return dataCacheStatic ?? (dataCacheStatic = dataCache.Value);
+            }
+            else
+            {
+                return dataCache.Value;
+            }
         }
 
         public IEnumerator<TRow> GetEnumerator()
         {
-            Logger.Log("CsvTableBase<{0}>.GetEnumerator", typeof(TRow).FullName);
-            return (isDataCached ? GetDataCached() : GetDataDirect()).GetEnumerator();
+            Logger.Log("CsvTableBase<{0}>.GetEnumerator cached:{1}", typeof(TRow).FullName, IsDataCached);
+            return (IsDataCached ? GetDataCached() : GetDataDirect()).GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
             return GetEnumerator();
-        }
-
-        public void OpenInNotepad()
-        {
-            Process.Start("notepad", this.FilePath);
         }
 
         /// <summary>
@@ -73,14 +80,21 @@ namespace CsvLINQPadDriver.CodeGen
         {
             CsvLINQPadDriver.Helpers.Logger.Log("{0}.Where({1},{2})", typeof(TRow).Name, propertyName, string.Join(",", values));
 
-            ILookup<string, TRow> propertyIndex;
-            if (!indexes.TryGetValue(propertyName, out propertyIndex))
+            if (IsDataCached)
             {
-                propertyIndex = this.ToLookup(getProperty, StringComparer.Ordinal);
-                indexes.Add(propertyName, propertyIndex);
+                ILookup<string, TRow> propertyIndex;
+                if (!indexes.TryGetValue(propertyName, out propertyIndex))
+                {
+                    propertyIndex = this.ToLookup(getProperty, StringComparer.Ordinal);
+                    indexes.Add(propertyName, propertyIndex);
+                }
+                var result = values.SelectMany(value => propertyIndex[value]);
+                return values.Length > 1 ? result.Distinct() : result;
             }
-            var result = values.SelectMany(value => propertyIndex[value]);
-            return values.Length > 1 ? result.Distinct() : result;
+            else
+            {
+                return this.Where(r => values.Contains(getProperty(r), StringComparer.Ordinal));
+            }
         }
     }
 }
