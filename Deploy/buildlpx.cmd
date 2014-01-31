ec0edfbf6ffa9495e3812c92d20ff471a4296de2
dobrou <dobrou@gmail.com>
2014-01-31 19:26:43 +0100

    Refactoring - rename

--- Deploy/buildlpx.cmd
+++ Deploy/buildlpx.cmd
@@ -1,11 +1,11 @@
-@set version=1.4
+@set version=1.5
 @set zip="%ProgramFiles%\7-Zip\7z.exe"
 @set output="CsvLINQPadDriver.v%version%.lpx"
 
 del %output%
 
-%zip% a -tzip "%output%" header.xml ..\Src\bin\Debug\CsvLINQPadDriver.dll ..\Src\bin\Debug\CsvHelper.dll ..\Src\bin\Debug\Connection.png ..\Src\bin\Debug\FailedConnection.png ..\README.md ..\Tools\CsvLINQPadFileOpen\bin\Debug\CsvLINQPadFileOpen.exe 
-copy /y ..\Tools\CsvLINQPadFileOpen\bin\Debug\CsvLINQPadFileOpen.exe CsvLINQPadFileOpen.v%version%.exe
+%zip% a -tzip "%output%" header.xml ..\bin\Debug\CsvLINQPadDriver.dll ..\bin\Debug\CsvHelper.dll ..\bin\Debug\Connection.png ..\bin\Debug\FailedConnection.png ..\README.md ..\bin\Debug\CsvLINQPadFileOpen.exe 
+copy /y ..\bin\Debug\CsvLINQPadFileOpen.exe CsvLINQPadFileOpen.v%version%.exe
 
 @echo Package %output% created.
 @pause
