376657f41b0fecfc1dd31aefc22d3e50cf5770c2
dobrou <dobrou@gmail.com>
2015-02-10 21:37:14 +0100

    Travis CI build fix

---
+++ Src/CsvLINQPadDriver/ConnectionDialog-NoWPF.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CsvLINQPadDriver
+{
+    public class ConnectionDialog
+    {
+        private CsvDataContextDriverProperties properties;
+
+        public ConnectionDialog(CsvDataContextDriverProperties properties)
+        {
+            this.properties = properties;
+        }
+
+        internal bool? ShowDialog()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
