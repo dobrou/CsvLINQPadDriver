7cf93dfd2e5d877b563a03428c0eb5f8452f869d
Ivan Ivon <ivan.ivon@zerto.com>
2021-05-05 15:12:13 +0300

    Use IsExternalInit package.

--- Src/CsvLINQPadDriver/System.Runtime.CompilerServices/IsExternalInit.cs
+++
@@ -1,6 +0,0 @@
-﻿// ReSharper disable once CheckNamespace
-namespace System.Runtime.CompilerServices
-{
-    // ReSharper disable once UnusedType.Global
-    internal static class IsExternalInit { }
-}
